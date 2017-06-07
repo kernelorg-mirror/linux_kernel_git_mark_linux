f3429ba1682be54d22508aba0cb8aca6bbeca7ba
Mark Rutland <mark.rutland@arm.com>
2017-06-07 14:47:52 +0100

    arm64: pad VMAP'd stacks to PAGE_SIZE
    VMAP'd stacks are allocated in the vmalloc area, at PAGE_SIZE
    granularity. Thus, it makes no sense to make them smaller than a page.
    
    This patch ensures that when VMAP_STACK is selected, THREAD_SIZE is at
    least PAGE_SIZE. When VMAP_STACK is not selected, the current 16K
    THREAD_SIZE is used.
    
    THREAD_SIZE and THREAD_SIZE order are now decoupled from specific
    COFNIG_ARM64_xK_PAGES configurations. To change the minimum THREAD_SIZE,
    it is only necessary to update MIN_THREAD_SHIFT, with THREAD_SIZE and
    THREAD_SIZE order calculated automatically (and padded as necessary).
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -23,13 +23,26 @@
 
 #include <linux/compiler.h>
 
-#ifdef CONFIG_ARM64_4K_PAGES
-#define THREAD_SIZE_ORDER	2
-#elif defined(CONFIG_ARM64_16K_PAGES)
-#define THREAD_SIZE_ORDER	0
+#include <asm/memory.h>
+#include <asm/page.h>
+
+#define MIN_THREAD_SHIFT	14
+
+/*
+ * Each VMAP stack is a separate VMALLOC allocation, which is at least
+ * PAGE_SIZE.
+ */
+#if defined(CONFIG_VMAP_STACK) && (MIN_THREAD_SHIFT < PAGE_SHIFT)
+#define THREAD_SHIFT		PAGE_SHIFT
+#else
+#define THREAD_SHIFT		MIN_THREAD_SHIFT
 #endif
 
-#define THREAD_SIZE		16384
+#if THREAD_SHIFT >= PAGE_SHIFT
+#define THREAD_SIZE_ORDER	(THREAD_SHIFT - PAGE_SHIFT)
+#endif
+
+#define THREAD_SIZE		(UL(1) << THREAD_SHIFT)
 #define THREAD_START_SP		(THREAD_SIZE - 16)
 
 #ifndef __ASSEMBLY__
