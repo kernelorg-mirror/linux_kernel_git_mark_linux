2d335f681c862ecc203e6e760dea38bacb323b61
Mark Rutland <mark.rutland@arm.com>
2020-01-07 18:17:47 +0000

    arm64: entry: move ARM64_ERRATUM_845719 workaround to C
    To make the entry code less of a rats nest of overlapping labels and
    shared state, and to make the code easier to debug and maintain, let's
    move the workaround for ARM64_ERRATUM_845719 to C.
    
    The workaround requires us to perform a write to CONTEXTIDR_EL1 at
    AArch64 EL1 before retuning to an AArch32 EL0 task. There are no
    additional requirements on the state of the CPU, or on subsequent
    instructions prior to the ERET, so this can safely be performed in C
    code.
    
    As with the assembly version, we preserve the value of CONTEXTIDR if
    CONFIG_PID_IN_CONTEXTIDR is selected.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Will Deacon <will@kernel.org>

--- arch/arm64/kernel/entry-common.c
+++ arch/arm64/kernel/entry-common.c
@@ -5,6 +5,7 @@
  * Copyright (C) 2019 ARM Ltd.
  */
 
+#include <linux/compat.h>
 #include <linux/context_tracking.h>
 #include <linux/hardirq.h>
 #include <linux/irq.h>
@@ -160,6 +161,21 @@ static void notrace el0_prepare_entry(struct pt_regs *regs)
 }
 NOKPROBE_SYMBOL(el0_prepare_entry);
 
+static void notrace workaround_arm64_erratum_845719(void)
+{
+	unsigned long val = 0;
+
+	if (!IS_ENABLED(CONFIG_ARM64_ERRATUM_845719) ||
+	    !cpus_have_const_cap(ARM64_WORKAROUND_845719) ||
+	    !is_compat_task())
+		return;
+
+	if (IS_ENABLED(CONFIG_PID_IN_CONTEXTIDR))
+		val = read_sysreg(contextidr_el1);
+	write_sysreg(val, contextidr_el1);
+}
+NOKPROBE_SYMBOL(workaround_arm64_erratum_845719);
+
 static void notrace el0_prepare_return(struct pt_regs *regs)
 {
 	unsigned long flags;
@@ -176,6 +192,8 @@ static void notrace el0_prepare_return(struct pt_regs *regs)
 		__enable_single_step_nosync();
 
 	user_enter();
+
+	workaround_arm64_erratum_845719();
 
 	stackleak_erase();
 }
