8aec17cec649c41bc80b0f95408d0bcb69f75f01
Mark Rutland <mark.rutland@arm.com>
2020-09-21 15:28:56 +0100

    arm64: uaccess: remove set_fs()
    Now that the uaccess primitives dont take addr_limit into acccount, we
    have no need to manipulate this via set_fs() and get_fs(). Remove
    support for these, along with some infrastructure this renders
    redundant.
    
    We no longer need to flip UAO to access kernel memory under KERNEL_DS,
    and head.S unconditionally clears UAO for all kernel configurations via
    an ERET in init_kernel_el. Thus, we don't need to dynamically flip UAO,
    nor do we need to context-switch it. However, we do need to clear UAO
    (and set PAN) during SDEI entry.
    
    Masking of __user pointers no longer needs to use the dynamic value of
    addr_limit, and can use a constant derived from the maximum possible
    userspace task size. A new TASK_SIZE_MAX constant is introduced for
    this, which is also used by core code. In configurations supporting
    52-bit VAs, this may include a region of unusable VA space above a
    48-bit TTBR0 limit, but never includes any portion of TTBR1.
    
    Note that TASK_SIZE_MAX is an exclusive limit, while USER_DS and
    KERNEL_DS were inclusive limits, and is converted to a mask by
    subtracting one.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: James Morse <james.morse@arm.com>
    Cc: Will Deacon <will@kernel.org>

--- arch/arm64/kernel/sdei.c
+++ arch/arm64/kernel/sdei.c
@@ -231,23 +231,18 @@ asmlinkage __kprobes notrace unsigned long
 __sdei_handler(struct pt_regs *regs, struct sdei_registered_event *arg)
 {
 	unsigned long ret;
-	mm_segment_t orig_addr_limit;
 
 	/*
 	 * We didn't take an exception to get here, so the HW hasn't
-	 * set/cleared bits in PSTATE that we may rely on. Intialize PAN/UAO,
-	 * then use force_uaccess_begin() to reset addr_limit.
+	 * set/cleared bits in PSTATE that we may rely on. Intialize PAN/UAO.
 	 */
 	__sdei_pstate_entry();
-	orig_addr_limit = force_uaccess_begin();
 
 	nmi_enter();
 
 	ret = _sdei_handler(regs, arg);
 
 	nmi_exit();
-
-	force_uaccess_end(orig_addr_limit);
 
 	return ret;
 }
