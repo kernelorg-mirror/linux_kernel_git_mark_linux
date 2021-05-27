e74af6ad8d600cb3366353013d6ba83be2d4841c
Mark Rutland <mark.rutland@arm.com>
2021-05-27 11:23:35 +0100

    arm64: patching: unify stop_machine() patch synchronization
    Some instruction sequences cannot be safely modified while they may be
    concurrently executed, and so it's necessary to temporarily stop all
    CPUs while performing the modification. We have separate implementations
    of this for alternatives and kprobes.
    
    This patch unifies these with a common patch_machine() helper function
    which handles the necessary synchronization to ensure that CPUs are
    stopped during patching. This separates the patching logic, making it
    easier to understand, and means that we only have to maintain one
    synchronization algorithm.
    
    The synchronization logic in do_patch_machine() only uses unpatchable
    functions, and the function itself is marked `noinstr` to prevent
    instrumentation. The patch_machine() helper is left instrumentatble as
    stop_machine() is instrumentable, and therefore there is no benefit to
    forbidding instrumentation.
    
    As with the prior alternative patching sequence, the CPU to apply the
    patch is chosen early so that this may be deterministic.
    
    Since __apply_alternatives_stopped() is only ever called once under
    apply_alternatives_all(), the `all_alternatives_applied` variable and
    warning are redundant and therefore removed.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Andre Przywara <andre.przywara@arm.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Cc: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/patching.h
+++ arch/arm64/include/asm/patching.h
@@ -10,4 +10,8 @@ int aarch64_insn_write(void *addr, u32 insn);
 int aarch64_insn_patch_text_nosync(void *addr, u32 insn);
 int aarch64_insn_patch_text(void *addrs[], u32 insns[], int cnt);
 
+typedef int (*patch_machine_func_t)(void *);
+int patch_machine_cpuslocked(patch_machine_func_t func, void *arg);
+int patch_machine(patch_machine_func_t func, void *arg);
+
 #endif	/* __ASM_PATCHING_H */
