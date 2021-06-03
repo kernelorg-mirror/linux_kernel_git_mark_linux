9aa2e9d669aa544529f66cba2fe5c4e414ea9dba
Mark Rutland <mark.rutland@arm.com>
2021-06-03 11:38:19 +0100

    arm64: alternative: make clean_dcache_range_nopatch() safe
    Currently clean_dcache_range_nopatch() can be instrumented, and also
    calls out to non-inlined functions which can be patched and/or
    instrumented. Due to this, it's possible that it calls a function which
    is in a partially patched state, which is not safe.
    
    Let's fix this by marking it `noinstr` to prevent instrumentation, and
    replacing the call to read_sanitised_ftr_reg(SYS_CTR_EL0) with a direct
    read of arm64_ftr_reg_ctrel0.sys_val. The latter avoids a fair amount of
    code including a bsearch().
    
    Since cpuid_feature_extract_unsigned_field() is marked __always_inline,
    this is safe to use.
    
    Other than the improved robustness, there should be no functional change
    as a result of this patch.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Andre Przywara <andre.przywara@arm.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Cc: Will Deacon <will@kernel.org>

--- arch/arm64/kernel/alternative.c
+++ arch/arm64/kernel/alternative.c
@@ -115,11 +115,11 @@ static void patch_alternative(struct alt_instr *alt,
  * accidentally call into the cache.S code, which is patched by us at
  * runtime.
  */
-static void clean_dcache_range_nopatch(u64 start, u64 end)
+static void noinstr clean_dcache_range_nopatch(u64 start, u64 end)
 {
 	u64 cur, d_size, ctr_el0;
 
-	ctr_el0 = read_sanitised_ftr_reg(SYS_CTR_EL0);
+	ctr_el0 = arm64_ftr_reg_ctrel0.sys_val;
 	d_size = 4 << cpuid_feature_extract_unsigned_field(ctr_el0,
 							   CTR_DMINLINE_SHIFT);
 	cur = start & ~(d_size - 1);
