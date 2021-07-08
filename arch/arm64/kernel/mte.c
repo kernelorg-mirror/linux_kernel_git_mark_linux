d78565f5906b409be0c92b75ee971ab7983b8a5f
Mark Rutland <mark.rutland@arm.com>
2021-07-08 18:36:59 +0100

    arm64: kasan: mte: remove redundant mte_report_once logic
    We have special logic to suppress MTE tag check fault reporting, based
    on a global `mte_report_once` and `reported` variables. These can be
    used to suppress calling kasan_report() when taking a tag check fault,
    but do not prevent taking the fault in the first place, nor does they
    affect the way we disable tag checks upon taking a fault.
    
    The core KASAN code already defaults to reporting a single fault, and
    has a `multi_shot` control to permit reporting multiple faults. The only
    place we transiently alter `mte_report_once` is in lib/test_kasan.c,
    where we also the `multi_shot` state as the same time. Thus
    `mte_report_once` and `reported` are redundant, and can be removed.
    
    When a tag check fault is taken, tag checking will be disabled by
    `do_tag_recovery` and must be explicitly re-enabled if desired. The test
    code does this by calling kasan_enable_tagging_sync().
    
    This patch removes the redundant mte_report_once() logic and associated
    variables.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Konovalov <andreyknvl@gmail.com>
    Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>

--- arch/arm64/kernel/mte.c
+++ arch/arm64/kernel/mte.c
@@ -22,8 +22,6 @@
 #include <asm/ptrace.h>
 #include <asm/sysreg.h>
 
-static bool report_fault_once = true;
-
 #ifdef CONFIG_KASAN_HW_TAGS
 /* Whether the MTE asynchronous mode is enabled. */
 DEFINE_STATIC_KEY_FALSE(mte_async_mode);
@@ -137,16 +135,6 @@ void mte_enable_kernel_async(void)
 		static_branch_enable(&mte_async_mode);
 }
 #endif
-
-void mte_set_report_once(bool state)
-{
-	WRITE_ONCE(report_fault_once, state);
-}
-
-bool mte_report_once(void)
-{
-	return READ_ONCE(report_fault_once);
-}
 
 #ifdef CONFIG_KASAN_HW_TAGS
 void mte_check_tfsr_el1(void)
