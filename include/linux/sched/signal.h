c85a1f80f7fb350454e647da939ebd6a9028dc6d
Mark Rutland <mark.rutland@arm.com>
2021-11-01 16:50:48 +0000

    HACK: detect unretriable fatal kernel uaccess faults
    Lockups have been reported where it appears that a put_user() in
    schedule_tail() never makes forward progress. This *might* happen if we
    had a fatal signal pending for a kernel uaccess fault with none of the
    following flags:
    
    * VM_FAULT_ERROR
    * VM_FAULT_BADMAP
    * VM_FAULT_BADACCESS
    
    Add an explicit check for this case so we can try to trigger it while
    fuzzing. Dump the fault flags to aid debugging.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>

--- include/linux/sched/signal.h
+++ include/linux/sched/signal.h
@@ -397,9 +397,14 @@ static inline int signal_pending_state(unsigned int state, struct task_struct *p
 static inline bool fault_signal_pending(vm_fault_t fault_flags,
 					struct pt_regs *regs)
 {
-	return unlikely((fault_flags & VM_FAULT_RETRY) &&
-			(fatal_signal_pending(current) ||
-			 (user_mode(regs) && signal_pending(current))));
+	bool retry = fault_flags & VM_FAULT_RETRY;
+	bool fatal = fatal_signal_pending(current);
+	bool user = user_mode(regs);
+	bool pending = signal_pending(current);
+
+	WARN(fatal && !user && !retry, "Fatal non-retriable kernel fault with flags %u\n", fault_flags);
+
+	return unlikely(retry && (fatal || (user && pending)));
 }
 
 /*
