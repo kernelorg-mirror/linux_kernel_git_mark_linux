5cd28a6930b5197667b219f135307cac7331aaa3
Mark Rutland <mark.rutland@arm.com>
2022-09-05 14:20:33 +0100

    arm64: insn: add tests for ADR/ADRP manipulation
    Recently bugs have been identified in the arm64 insn manipulation
    functions. To prevent such issues in future, we'd like tests for the
    insn manipulation functions.
    
    This patch adds some infrastructure for testing the arm64 insn
    manipulation functions, along with some initial tests covering the ADR
    and ADRP instructions.
    
    Subsequent patches will add additional tests and test suites.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Joey Gouly <joey.gouly@arm.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Will Deacon <will@kernel.org>

---
+++ arch/arm64/lib/test_insn.c
@@ -0,0 +1,269 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * KUnit tests for arm64 instruction manipulation code.
+ *
+ * Copyright (C) 2022 Arm Ltd.
+ */
+
+#include <kunit/test.h>
+
+#include <linux/module.h>
+#include <linux/sizes.h>
+#include <linux/types.h>
+
+#include <asm/byteorder.h>
+#include <asm/insn.h>
+
+#define __ASM_U32(opcode, insn, inputs...)				\
+({									\
+	extern __le32 opcode;						\
+	asm(								\
+	"	.subsection 1\n"					\
+	"	.globl " __stringify(opcode) "\n"			\
+	__stringify(opcode) ":\n"					\
+		insn "\n"						\
+	"	.previous\n"						\
+	:								\
+	: inputs							\
+	);								\
+	le32_to_cpu(opcode);						\
+})
+
+#define ASM_U32(insn, inputs...)					\
+	__ASM_U32(__UNIQUE_ID(opcode), insn, inputs)
+
+/*
+ * Map from x<N> or w<N> to AARCH64_INSN_REG_<n>
+ *
+ * This code is really good
+ */
+#define AARCH64_REG_IDX_x0	AARCH64_INSN_REG_0
+#define AARCH64_REG_IDX_x1	AARCH64_INSN_REG_1
+#define AARCH64_REG_IDX_x2	AARCH64_INSN_REG_2
+#define AARCH64_REG_IDX_x3	AARCH64_INSN_REG_3
+#define AARCH64_REG_IDX_x4	AARCH64_INSN_REG_4
+#define AARCH64_REG_IDX_x5	AARCH64_INSN_REG_5
+#define AARCH64_REG_IDX_x6	AARCH64_INSN_REG_6
+#define AARCH64_REG_IDX_x7	AARCH64_INSN_REG_7
+#define AARCH64_REG_IDX_x8	AARCH64_INSN_REG_8
+#define AARCH64_REG_IDX_x9	AARCH64_INSN_REG_9
+#define AARCH64_REG_IDX_x10	AARCH64_INSN_REG_10
+#define AARCH64_REG_IDX_x11	AARCH64_INSN_REG_11
+#define AARCH64_REG_IDX_x12	AARCH64_INSN_REG_12
+#define AARCH64_REG_IDX_x13	AARCH64_INSN_REG_13
+#define AARCH64_REG_IDX_x14	AARCH64_INSN_REG_14
+#define AARCH64_REG_IDX_x15	AARCH64_INSN_REG_15
+#define AARCH64_REG_IDX_x16	AARCH64_INSN_REG_16
+#define AARCH64_REG_IDX_x17	AARCH64_INSN_REG_17
+#define AARCH64_REG_IDX_x18	AARCH64_INSN_REG_18
+#define AARCH64_REG_IDX_x19	AARCH64_INSN_REG_19
+#define AARCH64_REG_IDX_x20	AARCH64_INSN_REG_20
+#define AARCH64_REG_IDX_x21	AARCH64_INSN_REG_21
+#define AARCH64_REG_IDX_x22	AARCH64_INSN_REG_22
+#define AARCH64_REG_IDX_x23	AARCH64_INSN_REG_23
+#define AARCH64_REG_IDX_x24	AARCH64_INSN_REG_24
+#define AARCH64_REG_IDX_x25	AARCH64_INSN_REG_25
+#define AARCH64_REG_IDX_x26	AARCH64_INSN_REG_26
+#define AARCH64_REG_IDX_x27	AARCH64_INSN_REG_27
+#define AARCH64_REG_IDX_x28	AARCH64_INSN_REG_28
+#define AARCH64_REG_IDX_x29	AARCH64_INSN_REG_29
+#define AARCH64_REG_IDX_x30	AARCH64_INSN_REG_30
+#define AARCH64_REG_IDX_xzr	AARCH64_INSN_REG_ZR
+
+#define AARCH64_REG_IDX_sp	AARCH64_INSN_REG_SP
+
+#define AARCH64_REG_IDX_w0	AARCH64_INSN_REG_0
+#define AARCH64_REG_IDX_w1	AARCH64_INSN_REG_1
+#define AARCH64_REG_IDX_w2	AARCH64_INSN_REG_2
+#define AARCH64_REG_IDX_w3	AARCH64_INSN_REG_3
+#define AARCH64_REG_IDX_w4	AARCH64_INSN_REG_4
+#define AARCH64_REG_IDX_w5	AARCH64_INSN_REG_5
+#define AARCH64_REG_IDX_w6	AARCH64_INSN_REG_6
+#define AARCH64_REG_IDX_w7	AARCH64_INSN_REG_7
+#define AARCH64_REG_IDX_w8	AARCH64_INSN_REG_8
+#define AARCH64_REG_IDX_w9	AARCH64_INSN_REG_9
+#define AARCH64_REG_IDX_w10	AARCH64_INSN_REG_10
+#define AARCH64_REG_IDX_w11	AARCH64_INSN_REG_11
+#define AARCH64_REG_IDX_w12	AARCH64_INSN_REG_12
+#define AARCH64_REG_IDX_w13	AARCH64_INSN_REG_13
+#define AARCH64_REG_IDX_w14	AARCH64_INSN_REG_14
+#define AARCH64_REG_IDX_w15	AARCH64_INSN_REG_15
+#define AARCH64_REG_IDX_w16	AARCH64_INSN_REG_16
+#define AARCH64_REG_IDX_w17	AARCH64_INSN_REG_17
+#define AARCH64_REG_IDX_w18	AARCH64_INSN_REG_18
+#define AARCH64_REG_IDX_w19	AARCH64_INSN_REG_19
+#define AARCH64_REG_IDX_w20	AARCH64_INSN_REG_20
+#define AARCH64_REG_IDX_w21	AARCH64_INSN_REG_21
+#define AARCH64_REG_IDX_w22	AARCH64_INSN_REG_22
+#define AARCH64_REG_IDX_w23	AARCH64_INSN_REG_23
+#define AARCH64_REG_IDX_w24	AARCH64_INSN_REG_24
+#define AARCH64_REG_IDX_w25	AARCH64_INSN_REG_25
+#define AARCH64_REG_IDX_w26	AARCH64_INSN_REG_26
+#define AARCH64_REG_IDX_w27	AARCH64_INSN_REG_27
+#define AARCH64_REG_IDX_w28	AARCH64_INSN_REG_28
+#define AARCH64_REG_IDX_w29	AARCH64_INSN_REG_29
+#define AARCH64_REG_IDX_w30	AARCH64_INSN_REG_30
+#define AARCH64_REG_IDX_wzr	AARCH64_INSN_REG_ZR
+
+#define REG_IDX(r)	(AARCH64_REG_IDX_##r)
+
+#define INSN_MSG(insn)							\
+	KUNIT_SUBSUBTEST_INDENT "'" #insn "' is 0x%08x", insn
+
+#define INSN_EXPECT_IS(test, type, insn)				\
+	KUNIT_EXPECT_TRUE_MSG(test, aarch64_insn_is_##type(insn), 	\
+			      INSN_MSG(insn))
+
+#define INSN_EXPECT_IS_NOT(test, type, insn)				\
+	KUNIT_EXPECT_FALSE_MSG(test, aarch64_insn_is_##type(insn), 	\
+			       INSN_MSG(insn))
+
+struct test_insn_adr_adrp_params {
+	enum aarch64_insn_adr_type type;
+	enum aarch64_insn_register rd;
+	s64 offset;
+
+	bool fail;
+	u32 insn;
+
+	bool (*is)(u32);
+	s64 (*get_offset)(u32);
+};
+
+static void test_insn_adr_adrp_case(struct kunit *test,
+				    struct test_insn_adr_adrp_params *params)
+{
+	enum aarch64_insn_register rd;
+	s64 offset;
+
+	u32 insn = aarch64_insn_gen_adr(0, params->offset, params->rd,
+					params->type);
+
+	if (params->fail) {
+		KUNIT_EXPECT_EQ(test, insn, AARCH64_BREAK_FAULT);
+		return;
+	}
+
+	KUNIT_EXPECT_EQ(test, insn, params->insn);
+
+	KUNIT_EXPECT_TRUE(test, params->is(insn));
+
+	rd = aarch64_insn_decode_register(AARCH64_INSN_REGTYPE_RD, insn);
+	KUNIT_EXPECT_EQ(test, rd, params->rd);
+
+	offset = params->get_offset(insn);
+	KUNIT_EXPECT_EQ(test, offset, params->offset);
+}
+
+#define __CASE_ADR(_rd, _offset, _fail, _insn)				\
+	{								\
+		.type = AARCH64_INSN_ADR_TYPE_ADR,			\
+		.is = aarch64_insn_is_adr,				\
+		.get_offset = aarch64_insn_adr_get_offset,		\
+		.rd = REG_IDX(_rd),					\
+		.offset = (_offset),					\
+		.fail = (_fail),					\
+		.insn = (_insn),					\
+	}
+
+#define CASE_ADR_GOOD(_rd, _offset)					\
+	__CASE_ADR(_rd, _offset, false,					\
+		   ASM_U32("adr " #_rd ", %0", "i" (_offset)))
+
+#define CASE_ADR_FAIL(_rd, _offset)					\
+	__CASE_ADR(_rd, _offset, true, 0)
+
+static void test_insn_adr(struct kunit *test)
+{
+	struct test_insn_adr_adrp_params params[] = {
+		CASE_ADR_GOOD(x0, 0),
+		CASE_ADR_GOOD(x1, 0),
+		CASE_ADR_GOOD(x2, 257),
+		CASE_ADR_GOOD(x3, -923),
+		CASE_ADR_GOOD(x4, SZ_512K),
+		CASE_ADR_GOOD(x5, -SZ_512K),
+		CASE_ADR_GOOD(x6, SZ_1M - 1),
+		CASE_ADR_GOOD(x7, -SZ_1M),
+
+		/* out-of-range immediates */
+		CASE_ADR_FAIL(x0, SZ_1M),
+		CASE_ADR_FAIL(x0, -SZ_1M - 1),
+	};
+
+	for (int i = 0; i < ARRAY_SIZE(params); i++) {
+		test_insn_adr_adrp_case(test, &params[i]);
+	}
+}
+
+/*
+ * For the asm, Ideally we'd use ". + %[imm]", but unfortuantely this or
+ * ":pghi_21_nc: . + %[imm]" causes binutils 2.38 to generate unexpected ELF
+ * notes, causing the build to fail with warnings of the form:
+ *
+ * | `.note.gnu.property' referenced in section `.text' of
+ * | arch/arm64/lib/test_insn.o: defined in discarded section
+ * | `.note.gnu.property' of arch/arm64/lib/test_insn.o
+ *
+ * Using a local label happens to inhibit this, and is functionally equivalent.
+ */
+#define CASE_ADRP(_rd, _offset, _fail, _insn)				\
+	{								\
+		.type = AARCH64_INSN_ADR_TYPE_ADRP,			\
+		.is = aarch64_insn_is_adrp,				\
+		.get_offset = aarch64_insn_adrp_get_offset,		\
+		.rd = REG_IDX(_rd),					\
+		.offset = (_offset),					\
+		.fail = (_fail),					\
+		.insn = (_insn),					\
+	}
+
+#define CASE_ADRP_GOOD(_rd, _offset)					\
+	CASE_ADRP(_rd, _offset, false,					\
+		  ASM_U32("1: adrp " #_rd ", 1b + %0",			\
+			  "i" ((s64)(_offset))))
+
+#define CASE_ADRP_FAIL(_rd, _offset)					\
+	CASE_ADRP(_rd, _offset, true, 0)
+
+static void test_insn_adrp(struct kunit *test)
+{
+	struct test_insn_adr_adrp_params params[] = {
+		CASE_ADRP_GOOD(x0,  0),
+		CASE_ADRP_GOOD(x1,  0),
+		CASE_ADRP_GOOD(x2,  SZ_4K),
+		CASE_ADRP_GOOD(x3,  -SZ_4K),
+		CASE_ADRP_GOOD(x4,  SZ_1G),
+		CASE_ADRP_GOOD(x5,  -SZ_1G),
+		CASE_ADRP_GOOD(x6,  SZ_2G),
+		CASE_ADRP_GOOD(x7,  -SZ_2G),
+		CASE_ADRP_GOOD(x8,  SZ_2G + SZ_1G),
+		CASE_ADRP_GOOD(x9,  -SZ_2G + -SZ_1G),
+		CASE_ADRP_GOOD(x10, SZ_4G - SZ_4K),
+		CASE_ADRP_GOOD(x11, -SZ_4G),
+
+		/* out-of-range immediates */
+		CASE_ADRP_FAIL(x0, SZ_4G),
+		CASE_ADRP_FAIL(x0, -SZ_4G - SZ_4K),
+	};
+
+	for (int i = 0; i < ARRAY_SIZE(params); i++) {
+		test_insn_adr_adrp_case(test, &params[i]);
+	}
+}
+
+static struct kunit_case aarch64_insn_insn_test_cases[] = {
+	KUNIT_CASE(test_insn_adr),
+	KUNIT_CASE(test_insn_adrp),
+	{ /* sentinel */ }
+};
+
+static struct kunit_suite test_aarch64_insn_insn_suite = {
+	.name = "aarch64_insn_instruction",
+	.test_cases = aarch64_insn_insn_test_cases,
+};
+
+kunit_test_suites(
+	&test_aarch64_insn_insn_suite
+);
+
+MODULE_LICENSE("GPL");
