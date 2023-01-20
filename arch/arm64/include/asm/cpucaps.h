ef23d1c728d0717e8138d51311e6f104d77eabde
Mark Rutland <mark.rutland@arm.com>
2023-01-20 17:16:04 +0000

    arm64: use cpus_have_cap() in has_useable_cnp()
    There's no point using cpus_have_const_cap() to check for
    ARM64_WORKAROUND_NVIDIA_CARMEL_CNP given that this is going to fall back
    to the bitmap check for all early CPUs, and even for late CPUs this
    isn't a fast path.
    
    Use cpus_have_cap() to always test the bitmap instead. At the same time,
    update cpucap_is_compiletime_possible() to avoid the check when it is
    not possible for ARM64_WORKAROUND_NVIDIA_CARMEL_CNP to be set.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Suzuki Poulose <suzuki.poulose@arm.com>
    Cc: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -27,6 +27,8 @@ cpucap_is_compiletime_possible(unsigned int cap)
 		return false;
 
 	switch (cap) {
+	case ARM64_WORKAROUND_NVIDIA_CARMEL_CNP:
+		return IS_ENABLED(CONFIG_NVIDIA_CARMEL_CNP_ERRATUM);
 	default:
 		return true;
 	}
