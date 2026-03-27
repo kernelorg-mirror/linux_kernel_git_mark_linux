2e0d4a37f793a38d26dcda63a3593c9fcfd641a6
Mark Rutland <mark.rutland@arm.com>
2026-03-27 18:09:33 +0000

    sched: dynamic: Simplify irqentry_exit_cond_resched()
    PREEMPT_DYNAMIC is now limited to the FULL and LAZY preemption models.
    In either model, irqentry_exit_cond_resched() is always called and never
    disabled.
    
    Remove the unnecessary code for this when PREEMPT_DYNAMIC is selected.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Tested-by: Mete Durlu <meted@linux.ibm.com>
    Tested-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Cc: Frederic Weisbecker <frederic@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: John Stultz <jstultz@google.com>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7824,9 +7824,6 @@ EXPORT_SYMBOL(__cond_resched_rwlock_write);
 # endif
 
 /*
- * SC:irqentry_exit_cond_resched
- *
- *
  * NONE:
  *   (unselectable)
  *
@@ -7834,11 +7831,9 @@ EXPORT_SYMBOL(__cond_resched_rwlock_write);
  *   (unselectable)
  *
  * FULL:
- *   irqentry_exit_cond_resched <- irqentry_exit_cond_resched
  *   dynamic_preempt_lazy       <- false
  *
  * LAZY:
- *   irqentry_exit_cond_resched <- irqentry_exit_cond_resched
  *   dynamic_preempt_lazy       <- true
  */
 
@@ -7882,14 +7877,12 @@ static void __sched_dynamic_update(int mode)
 {
 	switch (mode) {
 	case preempt_dynamic_full:
-		preempt_dynamic_enable(irqentry_exit_cond_resched);
 		preempt_dynamic_key_disable(preempt_lazy);
 		if (mode != preempt_dynamic_mode)
 			pr_info("Dynamic Preempt: full\n");
 		break;
 
 	case preempt_dynamic_lazy:
-		preempt_dynamic_enable(irqentry_exit_cond_resched);
 		preempt_dynamic_key_enable(preempt_lazy);
 		if (mode != preempt_dynamic_mode)
 			pr_info("Dynamic Preempt: lazy\n");
